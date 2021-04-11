01e3b9fa70de44676c31c920ad14cfed934ffca1
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-04-11 12:43:15 -0500

    updates to component

--- src/javascripts/components/homeComponent.js
+++ src/javascripts/components/homeComponent.js
@@ -1,13 +1,13 @@
-const homeComponent = (groupsWithUsersArray) => {
-  $('#main-content').append(`
+const homeComponent = (groupsWithUsersObj) => {
+  $('#main-container').append(`
     <div class="card" style="width: 18rem;">
-      <img src="${groupsWithUsersArray.image}" class="card-img-top" alt="${groupsWithUsersArray.name}">
+      <img src="${groupsWithUsersObj.image}" class="card-img-top" alt="${groupsWithUsersObj.name}">
       <div class="card-body">
-        <h5 class="card-title">${groupsWithUsersArray.name}</h5>
-        <p class="card-text">${groupsWithUsersArray.description}</p>
+        <h5 class="card-title">${groupsWithUsersObj.name}</h5>
+        <p class="card-text">${groupsWithUsersObj.description}</p>
       </div>
-      <ul id="groups-users" class="list-group list-group-flush">
-        <li class="list-group-item">An item</li>
+      <ul id="groups-users-${groupsWithUsersObj.id}" class="list-group list-group-flush">
+        <li class="list-group-item"><b>Group Members</b></li>
       </ul>
       <div class="card-body">
         <a href="#" class="card-link">Card link</a>
@@ -16,8 +16,8 @@ const homeComponent = (groupsWithUsersArray) => {
     </div>
   `);
 
-  groupsWithUsersArray.users.forEach((user) => {
-    $('#groups-users').append(`<li class="list-group-item">${user.first_name} ${user.last_name}</li>`);
+  groupsWithUsersObj.users.forEach((user) => {
+    $(`#groups-users-${groupsWithUsersObj.id}`).append(`<li class="list-group-item">${user.first_name} ${user.last_name}</li>`);
   });
 };
 
