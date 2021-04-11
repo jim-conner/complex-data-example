8d91a2d8d71244c22883df9a3c3063e4bbc0df51
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-04-11 16:04:09 -0500

    Update complete.js

--- src/javascripts/helpers/complete.js
+++ src/javascripts/helpers/complete.js
@@ -9,14 +9,8 @@ const groupsWithUsers = () => new Promise((resolve, reject) => {
   Promise.all([getUsers(), getGroups(), getUserGroups()])
     .then(([users, groups, userGroupsJoin]) => {
       console.warn(users, userGroupsJoin);
-      // // REMEMBER .map() returns an array
-      const allGroupInfoArray = groups.map((group) => {
-        // filter all the relationships looking in userGroupsJoin for the ones that apply to this specific group
-        const userInfoArray = [];
-        // Map through all of the relationships and finding the user
-        // Use the spread operator to create an object that includes the group info and all the users info
-        return { ...group, users: userInfoArray };
-      });
+      const allGroupInfoArray = [];
+
       resolve(allGroupInfoArray);
     }).catch((error) => reject(error));
 });
