0ec413a80ec7797006493a5e58cef2986ba90ee2
Jim Administrator <jamesdavidconner@gamil.com>
2021-04-13 18:23:31 -0500

    added 1st and 2nd examples from video

--- src/javascripts/helpers/complete.js
+++ src/javascripts/helpers/complete.js
@@ -4,13 +4,48 @@ import { getGroups } from './data/groupData';
 import getUsers from './data/userData';
 import getUserGroups from './data/userGroupsData';
 
+// 1ST EXAMPLE
+// const groupsWithUsers = () => new Promise((resolve, reject) => {
+//   Promise.all([getUsers(), getGroups(), getUserGroups()])
+//     .then(([users, groups, userGroupsJoin]) => {
+//       console.warn('users', users);
+//       const allGroupInfoArray = [];
+
+//       groups.forEach((group) => {
+//         const groupRelationshipsArray = [];
+//         const userInfoArray = [];
+//         // push all the relationships that apply to this group
+//         // use the spread to not have an array of arrays
+//         // instead we want an array of objects
+
+//         groupRelationshipsArray.push(...userGroupsJoin.filter((ug) => ug.group_id === group.id));
+//         console.warn('group Relationships', groupRelationshipsArray);
+
+//         // loop thru relationships, for each one, find the user that matches the groupRelationships id
+//         groupRelationshipsArray.forEach((groupRelationships) => {
+//           userInfoArray.push(users.find((user) => user.id === groupRelationships.user_id));
+//         });
+//         console.warn(userInfoArray);
+
+//         allGroupInfoArray.push({ ...group, users: userInfoArray });
+//       });
+
+//       console.warn(allGroupInfoArray);
+//       resolve(allGroupInfoArray);
+//     }).catch((error) => reject(error));
+// });
+
+// 2ND EXAMPLE
 const groupsWithUsers = () => new Promise((resolve, reject) => {
-  // COMPLETE THIS FUNCTION
   Promise.all([getUsers(), getGroups(), getUserGroups()])
     .then(([users, groups, userGroupsJoin]) => {
-      console.warn(users, userGroupsJoin);
-      const allGroupInfoArray = [];
+      const allGroupInfoArray = groups.map((group) => {
+        const groupRelationshipsArray = userGroupsJoin.filter((ug) => ug.group_id === group.id);
 
+        const userInfoArray = groupRelationshipsArray.map((groupRelationship) => users.find((user) => user.id === groupRelationship.user_id));
+
+        return { ...group, users: userInfoArray };
+      });
       resolve(allGroupInfoArray);
     }).catch((error) => reject(error));
 });
